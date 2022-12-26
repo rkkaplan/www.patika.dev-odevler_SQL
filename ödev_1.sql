53fca3dbc964fbcb4b63e832235f2c330b8aa5a7
rkkaplan <101667092+rkkaplan@users.noreply.github.com>
2022-12-26 21:54:35 +0300

    Add files via upload

---
+++ ödev_1.sql
@@ -0,0 +1,13 @@
+
+-- SELECT title, description FROM film
+
+-- SELECT * FROM film WHERE length > 60 AND length < 75
+-- SELECT * FROM film WHERE rental_rate = 0.99 AND ( replacement_cost = 12.99 OR replacement_cost = 28.99 ) 
+-- SELECT last_name, first_name FROM customer WHERE first_name = 'Mary'
+-- SELECT * FROM film WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99)
+-- SELECT title, length FROM film WHERE length >= 90 AND length <= 120
+-- SELECT title, length FROM film WHERE length BETWEEN 90 AND 120
+-- SELECT title, length FROM film WHERE length NOT BETWEEN 90 AND 120
+-- SELECT title, length FROM film WHERE length IN (70, 50, 60, 83)
+-- SELECT title, length FROM film WHERE length NOT IN (70, 50, 60)
+
