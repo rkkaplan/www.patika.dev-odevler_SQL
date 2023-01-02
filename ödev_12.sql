5e5a56bbc3a9588a47cc471bf14d4937f7b319df
rkkaplan <101667092+rkkaplan@users.noreply.github.com>
2023-01-02 23:14:48 +0300

    Add files via upload

---
+++ ödev_12.sql
@@ -0,0 +1,19 @@
+-- 1- film tablosunda film uzunluğu length sütununda gösterilmektedir. 
+--Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film WHERE length > (SELECT AVG (length) FROM film) 
+
+-- 2- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film WHERE rental_rate = (SELECT MAX(rental_rate) FROM film)
+
+-- 3- film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+INTERSECT
+SELECT * FROM film WHERE replacement_cost = (SELECT MIN(replacement_cost) FROM film)
+
+-- 4- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+SELECT payment.customer_id, customer.first_name, customer.last_name,  COUNT(payment.customer_id) AS YAPILAN_ALISVERIS_SAYISI
+FROM payment
+JOIN customer ON customer.customer_id = payment.customer_id
+GROUP BY customer.first_name, customer.last_name, payment.customer_id
+ORDER BY COUNT(payment.customer_id) DESC;
